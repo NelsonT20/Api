0ba4516c01885692f9b8b86dad3e1166b7f27bb0
NelsonT20 <nelsondaniel88@gmail.com>
2017-07-19 15:01:41 -0400

    cambio en el server.js a ES6

--- server/server.js
+++ server/server.js
@@ -1,26 +1,39 @@
 'use strict';
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+const loopback = require('loopback');
+const boot = require('loopback-boot');
 
-var app = module.exports = loopback();
+const app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
+app.start = (() => {
+
+  return app.listen(() => {
     app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
+    let baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      let explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
-};
+})
+
+// app.start = function () {
+//   // start the web server
+//   return app.listen(() => {
+//     app.emit('started');
+//     let baseUrl = app.get('url').replace(/\/$/, '');
+//     console.log('Web server listening at: %s', baseUrl);
+//     if (app.get('loopback-component-explorer')) {
+//       let explorerPath = app.get('loopback-component-explorer').mountPath;
+//       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+//     }
+//   });
+// };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
